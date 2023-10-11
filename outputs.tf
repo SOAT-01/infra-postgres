2068bdf7021bd65e269249191dc9c607e2cee648
Daniel Eiro <daniel.eiro.01@gmail.com>
2023-10-10 23:25:59 -0300

    build: initial files

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.fast_food.address
+  sensitive   = false
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.fast_food.port
+  sensitive   = false
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.fast_food.username
+  sensitive   = true
+}
+
