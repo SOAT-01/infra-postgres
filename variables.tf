9326274894aaa33f24e3c840ab9b052917200468
Daniel Eiro <daniel.eiro.01@gmail.com>
2023-10-13 22:53:09 -0300

    feat: adding TF State

--- variables.tf
+++ variables.tf
@@ -6,6 +6,11 @@ variable "region" {
   description = "AWS region"
 }
 
+variable "tf_bucket" {
+  description = "Bucket for Terraform State"
+  sensitive   = true
+}
+
 variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
