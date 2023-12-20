eecb37e055601f08452947466e0456fe6d9e2da1
Daniel Eiro <daniel.eiro.01@gmail.com>
2023-12-20 20:41:02 -0300

    feat: changing worflow to pass bucket as variable

--- versions.tf
+++ versions.tf
@@ -15,9 +15,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "tf-state-bucket-4567"
     key            = "rds/main.tf"
-    region         = "us-east-1"
     profile        = "default"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
